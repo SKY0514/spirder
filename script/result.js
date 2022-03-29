9365eada455645f3aa8057b111a0d6cea33d9705
giyoung <92899223+SKY0514@users.noreply.github.com>
2022-03-29 20:40:09 +0900

    temp 구조 틀과 색상변경

---
+++ script/result.js
@@ -0,0 +1,9 @@
+let problems = document.querySelector('.wrong_ul');
+
+if ($('.wrong_ul>.wrong_li1>p>span').text() == 1){
+  document.querySelector('.wrong_li1').style.backgroundColor = 'white'
+} else {
+  document.querySelector('.wrong_li1').style.backgroundColor = '#ffa'
+};
+
+
