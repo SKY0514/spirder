520afe2da8fb7bc58d622aa81c1f5b163831acb3
giyoung <92899223+SKY0514@users.noreply.github.com>
2022-04-04 21:52:38 +0900

    카카오톡 연결

--- static/js/kako_sr.js
+++ static/js/kako_sr.js
@@ -1,28 +1,119 @@
+
+
 function  kakoShare(){
-  Kakao.Link.sendDefault({
-  objectType: 'feed',
-  content: {
-    title: '오늘의 디저트',
-    description: '아메리카노, 빵, 케익',
-    imageUrl:
-      'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
-    link: {
-      mobileWebUrl: 'https://developers.kakao.com',
-      androidExecutionParams: 'test',
-    },
-  },
-  social: {
-    likeCount: 10,
-    commentCount: 20,
-    sharedCount: 30,
-  },
-  buttons: [
-    {
-      title: '웹으로 이동',
-      link: {
-        mobileWebUrl: 'https://developers.kakao.com',
+  const level = document.querySelector('.level')
+  if(level == 1){
+    Kakao.Link.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: 'MZ언어 테스트 결과',
+        description: '당신의 신조어 수준은 아직 태어나기 전. 스파이더스와 함께 새로 태어나요~',
+        imageUrl: url('../images/egg.png'),
+        link: {
+          mobileWebUrl: 'http://localhost:63342/04_05/templates/page/result_1.html?_ijt=bhvtd30tcoi5ppnfuvsmm88dtl&_ij_reload=RELOAD_ON_SAVE',
+          WebUrl: 'http://localhost:63342/04_05/templates/page/result_1.html?_ijt=bhvtd30tcoi5ppnfuvsmm88dtl&_ij_reload=RELOAD_ON_SAVE',
+        },
       },
-    }
-  ]
-});
+      buttons: [
+        {
+          title: '결과 확인하기',
+          link: {
+            mobileWebUrl: 'http://localhost:63342/04_05/templates/page/result_1.html?_ijt=bhvtd30tcoi5ppnfuvsmm88dtl&_ij_reload=RELOAD_ON_SAVE',
+            WebUrl: 'http://localhost:63342/04_05/templates/page/result_1.html?_ijt=bhvtd30tcoi5ppnfuvsmm88dtl&_ij_reload=RELOAD_ON_SAVE',
+          },
+        }
+      ]
+    });
+  }else if(level == 2){
+    Kakao.Link.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: 'MZ언어 테스트 결과',
+        description: '아 응애에요~ 당신의 신조어 수준은 신생아.',
+        imageUrl: url('../images/baby.png'),
+        link: {
+          mobileWebUrl: 'http://localhost:63342/04_05/templates/page/result_2.html?_ijt=bhvtd30tcoi5ppnfuvsmm88dtl&_ij_reload=RELOAD_ON_SAVE',
+          WebUrl: 'http://localhost:63342/04_05/templates/page/result_2.html?_ijt=bhvtd30tcoi5ppnfuvsmm88dtl&_ij_reload=RELOAD_ON_SAVE',
+        },
+      },
+      buttons: [
+        {
+          title: '결과 확인하기',
+          link: {
+            mobileWebUrl: 'http://localhost:63342/04_05/templates/page/result_2.html?_ijt=bhvtd30tcoi5ppnfuvsmm88dtl&_ij_reload=RELOAD_ON_SAVE',
+            WebUrl: 'http://localhost:63342/04_05/templates/page/result_2.html?_ijt=bhvtd30tcoi5ppnfuvsmm88dtl&_ij_reload=RELOAD_ON_SAVE',
+          },
+        }
+      ]
+    });
+  }else if(level == 3){
+    Kakao.Link.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: 'MZ언어 테스트 결과',
+        description: '당신은 분명 X세대! MZ세대와 소통하려면 좀 더 분발하세요~',
+        imageUrl: url('../images/삐삐.png'),
+        link: {
+          mobileWebUrl: 'http://localhost:63342/04_05/templates/page/result_3.html?_ijt=bhvtd30tcoi5ppnfuvsmm88dtl&_ij_reload=RELOAD_ON_SAVE',
+          WebUrl: 'http://localhost:63342/04_05/templates/page/result_3.html?_ijt=bhvtd30tcoi5ppnfuvsmm88dtl&_ij_reload=RELOAD_ON_SAVE',
+        },
+      },
+      buttons: [
+        {
+          title: '결과 확인하기',
+          link: {
+            mobileWebUrl: 'http://localhost:63342/04_05/templates/page/result_3.html?_ijt=bhvtd30tcoi5ppnfuvsmm88dtl&_ij_reload=RELOAD_ON_SAVE',
+            WebUrl: 'http://localhost:63342/04_05/templates/page/result_3.html?_ijt=bhvtd30tcoi5ppnfuvsmm88dtl&_ij_reload=RELOAD_ON_SAVE',
+          },
+        }
+      ]
+    });
+  }else if(level == 4){
+    Kakao.Link.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: 'MZ언어 테스트 결과',
+        description: '이젠 나도 유행어 알딱잘깔센',
+        imageUrl: url('../images/싸이월드.png'),
+        link: {
+          mobileWebUrl: 'http://localhost:63342/04_05/templates/page/result_4.html?_ijt=bhvtd30tcoi5ppnfuvsmm88dtl&_ij_reload=RELOAD_ON_SAVE',
+          WebUrl: 'http://localhost:63342/04_05/templates/page/result_4.html?_ijt=bhvtd30tcoi5ppnfuvsmm88dtl&_ij_reload=RELOAD_ON_SAVE',
+        },
+      },
+      buttons: [
+        {
+          title: '결과 확인하기',
+          link: {
+            mobileWebUrl: 'http://localhost:63342/04_05/templates/page/result_4.html?_ijt=bhvtd30tcoi5ppnfuvsmm88dtl&_ij_reload=RELOAD_ON_SAVE',
+            WebUrl: 'http://localhost:63342/04_05/templates/page/result_4.html?_ijt=bhvtd30tcoi5ppnfuvsmm88dtl&_ij_reload=RELOAD_ON_SAVE',
+          },
+        }
+      ]
+    });
+  }else if(level == 5){
+    Kakao.Link.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: 'MZ언어 테스트 결과',
+        description: '홀리몰리 과카몰리 로보카폴리 유행어 전문가',
+        imageUrl: url('../images/mz세대 이미지.jpg'),
+        link: {
+          mobileWebUrl: 'http://localhost:63342/04_05/templates/page/result_5.html?_ijt=bhvtd30tcoi5ppnfuvsmm88dtl&_ij_reload=RELOAD_ON_SAVE',
+          WebUrl: 'http://localhost:63342/04_05/templates/page/result_5.html?_ijt=bhvtd30tcoi5ppnfuvsmm88dtl&_ij_reload=RELOAD_ON_SAVE',
+        },
+      },
+      buttons: [
+        {
+          title: '결과 확인하기',
+          link: {
+            mobileWebUrl: 'http://localhost:63342/04_05/templates/page/result_5.html?_ijt=bhvtd30tcoi5ppnfuvsmm88dtl&_ij_reload=RELOAD_ON_SAVE',
+            WebUrl: 'http://localhost:63342/04_05/templates/page/result_5.html?_ijt=bhvtd30tcoi5ppnfuvsmm88dtl&_ij_reload=RELOAD_ON_SAVE',
+          },
+        }
+      ]
+    });
+  }
 }
+
+
+
